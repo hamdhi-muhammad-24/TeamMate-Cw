abd2a2b30cc1a5241260d1c52aa65f3bb30c89cb
hamdhi-muhammad-24 <hamdhimuhammad024@gmail.com>
2025-11-14 19:15:44 +0530

    Creating Club Member class

---
+++ src/teammate/model/ClubMember.java
@@ -0,0 +1,50 @@
+package teammate.model;
+
+public abstract class ClubMember {
+
+    protected String id;
+    protected String name;
+    protected String email;
+
+    public ClubMember() {
+    }
+
+    public ClubMember(String id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "ClubMember{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
